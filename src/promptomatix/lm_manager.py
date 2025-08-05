0359aa45c69fd1ebec80eab55d284403e83d6399
Risan Raja <risan.raja@xebia.com>
2025-08-05 15:54:06 +0530

    feat: add Bedrock support and model validation in LMManager

--- src/promptomatix/lm_manager.py
+++ src/promptomatix/lm_manager.py
@@ -10,6 +10,7 @@ class LMManager:
         'anthropic': Anthropic,
         'cohere': Cohere,
         # Add more providers as needed
+        'bedrock': dspy.LM
     }
 
     @classmethod
@@ -18,7 +19,7 @@ class LMManager:
                model_name: str, 
                api_key: str, 
                api_base: Optional[str] = None,
-               temperature: float = 0.7,
+               temperature: float = 0.0,
                max_tokens: int = 4000,
                **kwargs):
         """
@@ -48,7 +49,42 @@ class LMManager:
 
         # Get the appropriate LM class
         lm_class = cls.SUPPORTED_PROVIDERS[provider]
-        
+        bedrock_model_names = [
+            "us.anthropic.claude-3-haiku-20240307-v1:0",
+            "us.anthropic.claude-3-opus-20240229-v1:0",
+            "us.anthropic.claude-3-sonnet-20240229-v1:0",
+            "us.anthropic.claude-3-5-haiku-20241022-v1:0",
+            "us.anthropic.claude-3-5-sonnet-20240620-v1:0",
+            "us.anthropic.claude-3-5-sonnet-20241022-v2:0",
+            "us.anthropic.claude-3-7-sonnet-20250219-v1:0",
+            "us.anthropic.claude-opus-4-20250514-v1:0",
+            "us.anthropic.claude-sonnet-4-20250514-v1:0",
+            "us.deepseek.r1-v1:0",
+            "us.meta.llama4-maverick-17b-instruct-v1:0",
+            "us.meta.llama4-scout-17b-instruct-v1:0",
+            "us.meta.llama3-1-70b-instruct-v1:0",
+            "us.meta.llama3-1-8b-instruct-v1:0",
+            "us.meta.llama3-2-11b-instruct-v1:0",
+            "us.meta.llama3-2-1b-instruct-v1:0",
+            "us.meta.llama3-2-3b-instruct-v1:0",
+            "us.meta.llama3-2-90b-instruct-v1:0",
+            "us.meta.llama3-3-70b-instruct-v1:0",
+            "us.mistral.pixtral-large-2502-v1:0",
+            "us.amazon.nova-lite-v1:0",
+            "us.amazon.nova-micro-v1:0",
+            "us.amazon.nova-premier-v1:0",
+            "us.amazon.nova-pro-v1:0",
+        ]
+        if provider == 'bedrock':
+            if 'bedrock/' in model_name:
+                raise ValueError(f"Model name {model_name} is not a valid Bedrock model name. If model_name begins with 'bedrock/', please remove the 'bedrock/' prefix.")
+            if model_name not in bedrock_model_names:
+                raise Warning(f"Model name {model_name} is not a valid Bedrock model name. Please use one of the following models. Please use cross inference model ID.")
+            return dspy.LM(
+                model=f"bedrock/{model_name}",
+                max_tokens=max_tokens,
+                temperature=temperature,
+            )
         # Prepare base arguments
         lm_args = {
             "model": model_name,
